9f5b7b6c9557a88432c6dac84641ff43b3e2b650
SILVIO RAPHAEL COELHO <silvio.coelho@etec.sp.gov.br>
2019-02-01 17:38:24 -0200

    Métodos de lista

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require_once('config.php');
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+//Carrega uma lista de usuários 
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario com login e senha
+
+$usuario = new Usuario();
+$usuario->login("jose","123");
+echo $usuario;
